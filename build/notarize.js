5adc9c6bc6c852025719a27cec1c3d86cc1dad4d
RAGPIQ <patrick.white@ragpiq.com>
2025-07-31 16:19:03 +1000

    Enable macOS

---
+++ build/notarize.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const { notarize } = require('@electron/notarize');
+
+exports.default = async function notarizing(context) {
+  const { electronPlatformName, appOutDir } = context;
+  if (electronPlatformName !== 'darwin') return;
+
+  const appName = context.packager.appInfo.productFilename;
+
+  return await notarize({
+    appBundleId: process.env.NOTARIZE_APP_BUNDLE_ID,
+    appPath: `${appOutDir}/${appName}.app`,
+    appleId: process.env.APPLE_ID,
+    appleIdPassword: process.env.APPLE_APP_SPECIFIC_PASSWORD,
+    teamId: process.env.APPLE_TEAM_ID
+  });
+};
